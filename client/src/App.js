ee034d86dc2ccfc0bb129c235405208bc747eebf
ali <nademi@gmail.com>
2020-06-05 14:40:13 -0700

    component skeleton is up

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-// import Chat from './components/Chat/Chat';
-// import Join from './components/Join/Join';
-
+import Chat from "./components/Chat";
+import Join from "./components/Join";
+// props passed as querystring
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
-      {/* <Route path="/" exact component={Join} />
-      <Route path="/chat" component={Chat} /> */}
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
     </Router>
   );
 };
---
+++ client/src/components/Chat.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+const Chat = () => {
+  return <h1>Chat</h1>;
+};
+
+export default Chat;
